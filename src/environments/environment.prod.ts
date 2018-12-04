9dc67af2bd1353f9f47f7e86d30ce9f8e6f6a77e
Esmeralda Nava <ifglitterthen@gmail.com>
2018-12-04 03:34:45 -0600

    Añadiendo keys de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: false,
+  apiKey: 'AIzaSyBygdRkXhAzqCLuNYdh0r1sksqnQ09oQ-w',
+  authDomain: 'finlab-a4ee3.firebaseapp.com',
+  databaseURL: 'https://finlab-a4ee3.firebaseio.com',
+  projectId: 'finlab-a4ee3',
+  storageBucket: 'finlab-a4ee3.appspot.com',
+  messagingSenderId: '939897895686'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyBygdRkXhAzqCLuNYdh0r1sksqnQ09oQ-w',
+  authDomain: 'finlab-a4ee3.firebaseapp.com',
+  databaseURL: 'https://finlab-a4ee3.firebaseio.com',
+  projectId: 'finlab-a4ee3',
+  storageBucket: 'finlab-a4ee3.appspot.com',
+  messagingSenderId: '939897895686'
 };
 
 /*
